fc65608f455bc7b8dd1906cc06b21ef78163cc4a
KhoiHoang2410 <khoi2410@gmail.com>
2021-04-27 20:05:11 +0700

    Initial Commit

---
+++ Lab03/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Lab03
+//
+//  Created by Nguyen-Khoi Hoang on 27/04/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
