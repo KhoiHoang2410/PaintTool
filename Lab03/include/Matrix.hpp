5afbce9506cfb4ee05bce91a9f98eb7304489323
KhoiHoang2410 <khoi2410@gmail.com>
2021-04-28 22:01:31 +0700

    Add Matrix

--- Lab03/include/Matrix.hpp
+++ Lab03/include/Matrix.hpp
@@ -8,10 +8,22 @@
 #ifndef Matrix_hpp
 #define Matrix_hpp
 
+#include <vector>
+using namespace std;
+
 class Matrix {
 private:
+    int n, m;
+    vector<vector<int> > arr;
 public:
-    
+    Matrix(int n, int m);
+
+    void clear();
+    int& get(int i, int j);
+
+    Matrix operator *(const Matrix& src);
+    Matrix operator +(const Matrix& src);
+    Matrix operator -(const Matrix& src);
 };
 
 #endif /* Matrix_hpp */
--- Lab03/lib/Matrix.cpp
+++ Lab03/lib/Matrix.cpp
@@ -6,3 +6,63 @@
 //
 
 #include "Matrix.hpp"
+#include <iostream>
+
+Matrix::Matrix(int n, int m) {
+    this->n = n;
+    this->m = m;
+    arr.resize(n, vector<int>(m));
+}
+
+void Matrix::clear() {
+    for (int i=0; i<n; ++i)
+        for (int j=0; j<m; ++j)
+            arr[i][j] = 0;
+}
+
+int& Matrix::get(int i, int j) {
+    return arr[i][j];
+}
+
+Matrix Matrix::operator *(const Matrix& src) {
+    if (m != src.n) {
+        cout << "Error multiply matrix.\n";
+        exit(1);
+    }
+    
+    Matrix res(n, src.m);
+    for (int i=0; i<n; ++i)
+        for (int j=0; j<src.m; ++j)
+            for (int k=0; k< m; ++k)
+                res.arr[i][j] += arr[i][k] * src.arr[k][j];
+    
+    return res;
+}
+
+Matrix Matrix::operator +(const Matrix& src) {
+    if (m != src.m || n != src.n) {
+        cout << "Error plus matrix.\n";
+        exit(1);
+    }
+    
+    Matrix res(n, m);
+    for (int i=0; i<n; ++i)
+        for (int j=0; j<m; ++j)
+            res.arr[i][j] = arr[i][j] + src.arr[i][j];
+    
+    return res;
+}
+
+Matrix Matrix::operator -(const Matrix& src) {
+    if (m != src.m || n != src.n) {
+        cout << "Error minus matrix.\n";
+        exit(1);
+    }
+    
+    Matrix res(n, m);
+    for (int i=0; i<n; ++i)
+        for (int j=0; j<m; ++j)
+            res.arr[i][j] = arr[i][j] - src.arr[i][j];
+    
+    return res;
+}
